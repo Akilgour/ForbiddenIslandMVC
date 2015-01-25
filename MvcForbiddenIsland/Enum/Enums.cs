c379bea24a79b24d0cc27d6b032fc5a0876ec404
Akilgour <kilgourkilgour@hotmail.com>
2015-01-25 16:26:22 +0000

    Restarting the project, after long break, so just chickening it all in cus it builds

---
+++ MvcForbiddenIsland/Enum/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcForbiddenIsland.Enum
+{
+    public class Enums
+    {
+        public enum TileState
+        {
+            Normal,
+            Flodded,
+            Gone
+        }
+
+        public enum PlayerColour
+        {
+            Red,
+            Yellow,
+            Blue,
+            Black,
+            Green,
+            Grey,
+            None
+        }
+    }
+}
---
+++ MvcForbiddenIsland/Models/Interface/IIsland.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcForbiddenIsland.Models.Interface
+{
+   public interface IIsland
+    {
+        //List<IslandTile> IslandBoard { get; set; }
+    }
+}
