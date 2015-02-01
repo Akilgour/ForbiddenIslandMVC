18c077ed14ec1fac0a6a2173259ee3a6bb9cf0ad
Akilgour <kilgourkilgour@hotmail.com>
2015-02-01 18:31:37 +0000

    Addd guid to id value.  To player

--- MvcForbiddenIsland/Factory/PlayerListFactory.cs
+++ MvcForbiddenIsland/Factory/PlayerListFactory.cs
@@ -12,12 +12,12 @@ namespace MvcForbiddenIsland.Factory
         public List<Player> Create()
         {
             List<Player> playerList = new List<Player>();
-            playerList.Add(new Player() { Name = "Messenger", Colour = Enum.Enums.PlayerColour.Grey, Action = PlayerConstants.MESSENGER_ACTION });
-            playerList.Add(new Player() { Name = "Explorer", Colour = Enum.Enums.PlayerColour.Green, Action = PlayerConstants.EXPLORER_ACTION });
-            playerList.Add(new Player() { Name = "Diver", Colour = Enum.Enums.PlayerColour.Black, Action = PlayerConstants.DIVER_ACTION });
-            playerList.Add(new Player() { Name = "Pilot", Colour = Enum.Enums.PlayerColour.Blue, Action = PlayerConstants.PILOT_ACTION });
-            playerList.Add(new Player() { Name = "Navigator", Colour = Enum.Enums.PlayerColour.Yellow, Action = PlayerConstants.NAVIGATOR_ACTION });
-            playerList.Add(new Player() { Name = "Engineer", Colour = Enum.Enums.PlayerColour.Red, Action = PlayerConstants.ENGINEER_ACTION });
+            playerList.Add(new Player() { Id = Guid.NewGuid(), Name = "Messenger", Colour = Enum.Enums.PlayerColour.Grey, Action = PlayerConstants.MESSENGER_ACTION });
+            playerList.Add(new Player() { Id = Guid.NewGuid(), Name = "Explorer", Colour = Enum.Enums.PlayerColour.Green, Action = PlayerConstants.EXPLORER_ACTION });
+            playerList.Add(new Player() { Id = Guid.NewGuid(), Name = "Diver", Colour = Enum.Enums.PlayerColour.Black, Action = PlayerConstants.DIVER_ACTION });
+            playerList.Add(new Player() { Id = Guid.NewGuid(), Name = "Pilot", Colour = Enum.Enums.PlayerColour.Blue, Action = PlayerConstants.PILOT_ACTION });
+            playerList.Add(new Player() { Id = Guid.NewGuid(), Name = "Navigator", Colour = Enum.Enums.PlayerColour.Yellow, Action = PlayerConstants.NAVIGATOR_ACTION });
+            playerList.Add(new Player() { Id = Guid.NewGuid(), Name = "Engineer", Colour = Enum.Enums.PlayerColour.Red, Action = PlayerConstants.ENGINEER_ACTION });
             return playerList;
         }
     }
