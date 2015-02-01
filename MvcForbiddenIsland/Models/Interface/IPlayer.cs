3257cb9301de72ad55f8b6f3998b13d82427ecb1
Akilgour <kilgourkilgour@hotmail.com>
2015-02-01 16:42:48 +0000

    Added Player model

---
+++ MvcForbiddenIsland/Models/Interface/IPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcForbiddenIsland.Models.Interface
+{
+    public interface IPlayer
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Action { get; set; }
+        MvcForbiddenIsland.Enum.Enums.PlayerColour Colour { get; set; }
+    }
+}
---
+++ MvcForbiddenIsland/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using MvcForbiddenIsland.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcForbiddenIsland.Models
+{
+    public class Player : IPlayer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Action { get; set; }
+        public Enum.Enums.PlayerColour Colour { get; set; }
+    }
+}
