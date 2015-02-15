7bdfedd86b8c0c5bcb123c2df4662a3cd2708769
Akilgour <kilgourkilgour@hotmail.com>
2015-02-15 23:04:02 +0000

    Added entity framework stuff

---
+++ MvcForbiddenIsland/DAL/ForbiddenIslandContext.cs
@@ -0,0 +1,28 @@
+﻿using MvcForbiddenIsland.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MvcForbiddenIsland.DAL
+{
+    public class ForbiddenIslandContext : DbContext
+    {
+
+        public ForbiddenIslandContext() : base("ForbiddenIsland")
+        {
+        }
+
+        public DbSet<IslandTile> IslandTile { get; set; }
+        public DbSet<Player> Player { get; set; }
+    
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
