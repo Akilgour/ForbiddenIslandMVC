9e23cd4659e11b34fca26c5e3918554e666a62b5
Akilgour <kilgourkilgour@hotmail.com>
2015-02-24 22:13:25 +0000

    Added ValidationResults object and an interface for it

---
+++ MvcForbiddenIsland/Models/Interface/IValidationResults.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcForbiddenIsland.Models.Interface
+{
+    public interface IValidationResults
+    {
+        bool IsValid { get; set; }
+        string ErrorMessage { get; set; }
+    }
+}
---
+++ MvcForbiddenIsland/Models/ValidationResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcForbiddenIsland.Models
+{
+    public class ValidationResults : IValidationResults
+    {
+        public bool IsValid { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
