b7f9cc4a2de87784dcb7e3364df415f6cce5ad92
Akilgour <kilgourkilgour@hotmail.com>
2015-02-26 19:56:10 +0000

    Added GamePlaySettings

---
+++ MvcForbiddenIsland/Models/GamePlaySettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcForbiddenIsland.Models
+{
+    public class GamePlaySettings
+    {
+        public Guid fistMovePlayerId { get; set; }
+        public Guid secondMovePlayerId { get; set; }
+        public Guid thirdMovePlayerId { get; set; }
+        public Guid fourthMovePlayerId { get; set; }
+        public int moveNumber { get; set; }
+    }
+}
