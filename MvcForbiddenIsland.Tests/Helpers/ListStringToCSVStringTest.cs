90fc13f6bfc55c98c3cfb77f235c4cf931c4ca27
Akilgour <kilgourkilgour@hotmail.com>
2015-03-09 00:18:06 +0000

    Added a helper that turns a list of stings into a CSV list string with a "and" at the end for prettyness.

---
+++ MvcForbiddenIsland.Tests/Helpers/ListStringToCSVStringTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using MvcForbiddenIsland.Helpers;
+
+namespace MvcForbiddenIsland.Tests.Helpers
+{
+    [TestClass]
+    public class ListStringToCSVStringTest
+    {
+        [TestMethod]
+        public void NullListReurnStringEmpty()
+        {
+            //Arrange 
+            List<string> stringList = null;
+
+            //Act 
+            var results = ListStringToCSVString.ListToString(stringList);
+
+            //Assert
+            Assert.AreEqual(results, "");
+        }
+
+        [TestMethod]
+        public void NewListReurnStringEmpty()
+        {
+            //Arrange 
+            List<string> stringList = new List<string>();
+
+            //Act 
+            var results = ListStringToCSVString.ListToString(stringList);
+
+            //Assert
+            Assert.AreEqual(results, "");
+        }
+
+        [TestMethod]
+        public void ListWithOneItemReurnStringEmpty()
+        {
+            //Arrange 
+            List<string> stringList = new List<string>();
+            stringList.Add("A");
+            //Act 
+            var results = ListStringToCSVString.ListToString(stringList);
+
+            //Assert
+            Assert.AreEqual(results, "A");
+        }
+
+        [TestMethod]
+        public void ListWithTwoItemsReurnStringEmpty()
+        {
+            //Arrange 
+            List<string> stringList = new List<string>();
+            stringList.Add("A");
+            stringList.Add("B");
+
+            //Act 
+            var results = ListStringToCSVString.ListToString(stringList);
+
+            //Assert
+            Assert.AreEqual(results, "A and B");
+        }
+
+        [TestMethod]
+        public void ListWithThreeItemsReurnStringEmpty()
+        {
+            //Arrange 
+            List<string> stringList = new List<string>();
+            stringList.Add("A");
+            stringList.Add("B");
+            stringList.Add("C");
+
+            //Act 
+            var results = ListStringToCSVString.ListToString(stringList);
+
+            //Assert
+            Assert.AreEqual(results, "A, B and C");
+        }
+
+
+        [TestMethod]
+        public void ListWithThreeItemsOutOfOrderReurnStringEmpty()
+        {
+            //Arrange 
+            List<string> stringList = new List<string>();
+            stringList.Add("C");
+            stringList.Add("A");
+            stringList.Add("B");
+
+            //Act 
+            var results = ListStringToCSVString.ListToString(stringList);
+
+            //Assert
+            Assert.AreEqual(results, "A, B and C");
+        }
+    }
+}
---
+++ MvcForbiddenIsland/Helpers/ListStringToCSVString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcForbiddenIsland.Helpers
+{
+    public class ListStringToCSVString
+    {
+        public static string ListToString(List<string> stringList)
+        {
+            if (stringList == null)
+            {
+                return string.Empty;
+            }
+
+            string csvString = string.Join(", ", stringList.OrderBy(p => p.Substring(0)));
+            var commaIndex = csvString.LastIndexOf(",");
+
+            if (commaIndex != -1)
+            {
+                csvString = csvString.Remove(commaIndex, 1);
+                csvString = csvString.Insert(commaIndex, " and");
+            }
+
+            return csvString;
+        }
+
+    }
+}
