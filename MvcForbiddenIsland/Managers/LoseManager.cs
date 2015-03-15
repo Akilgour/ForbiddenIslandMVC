e3c27cfbee208af37945fa331ae24aad349db980
Akilgour <kilgourkilgour@hotmail.com>
2015-03-15 22:46:45 +0000

    Added Lose manager and moved the move manager test to its other folder

---
+++ MvcForbiddenIsland/Managers/LoseManager.cs
@@ -0,0 +1,21 @@
+﻿using MvcForbiddenIsland.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcForbiddenIsland.Managers
+{
+    public class LoseManager
+    {
+
+        public List<ValidationResults> HaveTheyLost(List<IslandTile> IslandBoard, int WaterLevel)
+        {
+
+            return null;
+
+        }
+
+
+    }
+}
