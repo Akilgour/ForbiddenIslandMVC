30434272c74177978377adb82ba61efb9ef8eede
Akilgour <kilgourkilgour@hotmail.com>
2015-03-20 18:14:47 +0000

    Added is can lost that will always return invalid

---
+++ MvcForbiddenIsland.Tests/Managers/Lose/Mocks/CanLose_NotValid.cs
@@ -0,0 +1,24 @@
+﻿using MvcForbiddenIsland.Models;
+using MvcForbiddenIsland.Validation.CanLose.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcForbiddenIsland.Tests.Managers.Lose.Mocks
+{
+   public class CanLose_NotValid : ICanLose
+    {
+       /// <summary>
+       /// This will always return a not valid
+       /// </summary>
+       /// <param name="IslandBoard"></param>
+       /// <param name="WaterLevel"></param>
+       /// <returns></returns>
+        public Models.ValidationResults IsValid(List<IslandTile> IslandBoard, int WaterLevel)
+        {
+            return new ValidationResults() { IsValid = false, ErrorMessage = LoseManagerTestConstants.CAN_LOSE_ERROR };
+        }
+    }
+}
